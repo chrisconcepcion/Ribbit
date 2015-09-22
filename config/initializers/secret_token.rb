84cdc7f5c297738eeeacfef785a61322efb638a5
christopherconcepcion <feverdoingwork@gmail.com>
2015-09-22 18:37:15 -0400

    initial ribbit app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ribbit::Application.config.secret_key_base = 'd3d900f51cfa6fafd379efb376f35710bc8fd7599cdb139f6e91cf9cb97566db51ab8296be6ba155fa460afef1beaebf5e27807e5383b2de842e17f728079fca'
