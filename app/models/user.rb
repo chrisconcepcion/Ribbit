fee1d38063765c5b9e4cca3580f3287da70d0b78
christopherconcepcion <feverdoingwork@gmail.com>
2015-09-24 17:28:19 -0400

    created user relationship infrastructure

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,25 @@ class User < ActiveRecord::Base
 	
 	
 	has_many :ribbits
+	has_many :follower_relationship, classname: "Relationship", foreign_key: "followed_id"
+	has_many :followed_relationship, classname: "Relationship", foreign_key: "follower_id"
+	has_many :followers, through: :follower_relationships
+	has_many :followeds, through: :followed_relationships
 	has_secure_password
 	validates :name, presence: true
 	validates :username, uniqueness: true, presence: true
 	validates :email, uniqueness: true, presence: true, format: { with: /\A[\w.+-]+@([\w]+.)+\w+\z/ }
 
+	
+	
+	def following? user
+    self.followeds.include? user
+	end
+ 
+	def follow user
+    Relationship.create follower_id: self.id, followed_id: user.id
+	end
+	
 	private
 	
 		def create_avatar_url
